4e9798c469b09260a931d4e8e7478c0bb3af79da
llik13 <murchick2004@gmail.com>
2024-11-25 04:08:16 +0200

    Delete projects

--- Catalog.BuisnesDataLayer/obj/Debug/net8.0/Catalog.BuisnesDataLayer.AssemblyInfo.cs
+++ Catalog.BuisnesDataLayer/obj/Debug/net8.0/Catalog.BuisnesDataLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.BuisnesDataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef9c3ed7ad6efa793fd80fea671402d89089f815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.BuisnesDataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.BuisnesDataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
